72982b44b10b37201164f9bbcd166e37a1746a57
Tyler Jones <tylereliasj@gmail.com>
2021-05-10 12:56:37 +0000

    loan calculations files

---
+++ calculations/indexed.py
@@ -0,0 +1,7 @@
+# The calculations for indexed linked loans (verðtryggð)
+# Credit goes to Jacky Mallett for providing the formula: https://github.com/jackymallett/indexed
+
+
+def index_calculation(amount, duration, interest, inflation):
+    ...
+    return []
